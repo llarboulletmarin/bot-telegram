175573858b8cf3ab582a32a158fefb7fd523d8f4
llarboulletmarin <l.larboulletmarin@gmail.com>
2024-04-12 15:39:40 +0200

    rename var

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram_bot import TelegramBot
 
 load_dotenv('.env')
 BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN')
-CHAT_ID = getenv('TELEGRAM_BOT_CHAT_ID')
+CHAT_ID = getenv('TELEGRAM_CHAT_ID')
 
 if __name__ == '__main__':
     bot = TelegramBot(BOT_TOKEN, CHAT_ID)
